4325f617389ebc03b1c8b9baa5a972477b307364
Thosea <104597976+ItsThosea@users.noreply.github.com>
2025-03-14 15:42:48 -0400

    debug time

--- grub-core/disk/cryptodisk.c
+++ grub-core/disk/cryptodisk.c
@@ -1248,6 +1248,7 @@ grub_cryptodisk_scan_device_real (const char *name,
                // for some reason time is only 7 or 6 for me, but just in case, get the ones place
                grub_uint64_t q = (time * 0xCCCCCCCCCCCCCCCDULL) >> 64; // divide by 10
                grub_uint64_t remainder = time - q * 10;
+               grub_printf_ (N_("DEBUG: %llu / %d, remainder = %llu / %d\n"), time, time, remainder, remainder);
                if (remainder < 7)
                  grub_printf_ (N_("Cannot boot: no efi on disk %s"), dev->uuid);
                else
